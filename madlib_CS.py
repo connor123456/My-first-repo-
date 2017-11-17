1b8ceca51d87ccb2083575e4d32e86444df9fc1d
connor123456 <33756645+connor123456@users.noreply.github.com>
2017-11-17 10:12:25 -0500

    first upload

---
+++ madlib_CS.py
@@ -0,0 +1,60 @@
+### Mad Libs ###
+### source: https://cdn-rainbowresource.netdna-ssl.com/products/032832i1.jpg
+
+print("Give me a plural noun")
+noun1 = input()
+
+print("Give me an adverb")
+adverb1 = input()
+
+print("Give me a verb")
+verb1 = input()
+
+print("Give me an article of clothing")
+clothing1 = input()
+
+print("Give me a body part")
+part1 = input()
+
+print("Give me an adjective")
+adjective1 = input()
+
+print("Give me a noun")
+noun2 =input()
+
+print("Give me a plural noun")
+noun3 =input()
+
+print("Give me a body part")
+part2 =input()
+
+print("Give me a plural noun")
+noun4 =input()
+
+print("Give me body part")
+part3 =input()
+
+print("Give me noun")
+noun5 =input()
+
+print("Give me noun")
+noun6 =input()
+
+print("Give me a verb ending in ing")
+ing1 =input()
+
+print("Give me an adjective")
+adjective2 =input()
+
+print("Give me an adjective")
+adjective3 =input()
+
+print("Give me verb")
+verb2 =input()
+
+
+### begin mad lib here ###
+
+print("Its simple. Turn the " + noun1 + ". Make him/her want " + adverb1 + "to  date you. Make sure your always dressed to" + 
+
+
