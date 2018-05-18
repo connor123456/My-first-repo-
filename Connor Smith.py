eac8d558840cfc508bfe72245cebcfea65d88590
connor123456 <33756645+connor123456@users.noreply.github.com>
2018-05-18 10:17:31 -0400

    Add files via upload

---
+++ Connor Smith.py
@@ -0,0 +1,13 @@
+import pyautogui as pg
+pg.hotkey('crtl','winleft','d')
+pg.hotkey('winleft')
+pg.typewrite('chrome\n',.2)
+
+pg.hotkey('winleft', 'up')
+time.sleep(1)
+pg.typewrite('espn.com\n',.2)
+
+
+
+
+
---
+++ Connor Smith2.py
@@ -0,0 +1,14 @@
+import pyautogui as pg
+import time
+pg.hotkey('crtl','winleft','d')
+pg.hotkey('winleft')
+pg.typewrite('chrome\n',.2)
+
+pg.hotkey('winleft', 'up')
+time.sleep(1)
+pg.typewrite('espn.com\n',.2)
+
+
+
+
+
